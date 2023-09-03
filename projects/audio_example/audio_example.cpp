9268ae8d453db6f2d9b106c38092e82161077215
knight.gao <no_man@163.com>
2023-09-03 22:12:16 +0800

    audio example

---
+++ projects/audio_example/audio_example.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+#include <BluetoothAudioSession.h>
+
+using namespace android::bluetooth::audio;
+
+int main()
+{
+    BluetoothAudioSession session( SessionType::A2DP_SOFTWARE_ENCODING_DATAPATH );
+    session.StartStream();
+
+    system( "pause" );
+    std::cout << "Hello World!\n";
+}
+
+
